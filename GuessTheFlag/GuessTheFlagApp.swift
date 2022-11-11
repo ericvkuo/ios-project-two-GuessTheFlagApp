bef6ffb132af76efd2880d0e24b344c7c17337c4
Eric Kuo <111590422+ericvkuo@users.noreply.github.com>
2022-11-12 01:06:08 +0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Eric Kuo on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
